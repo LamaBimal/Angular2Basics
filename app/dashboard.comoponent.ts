43dfd30479cc25967d47b554d43274d29a392328
lamabimal <bimaltamang1234@gmail.com>
2016-11-19 23:17:30 +0545

    Adding Basic routing examples..

---
+++ app/dashboard.comoponent.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by bimal on 11/19/16.
+ */
+import {Component} from '@angular/core';
+@Component({
+
+    selector:'my-dashboard',
+    template:'<h3>My Dashboard</h3>'
+})
+
+export class DashboardComponent{
+
+}
